ddd3218ce4fbec824bb6383e4369c55058e1c119
RAY <ray@wearhaus.com>
2015-08-06 15:22:46 -0700

    work on tablet interaction for sliders

--- static/js/custom.js
+++ static/js/custom.js
@@ -44,54 +44,118 @@ $(document).ready(function() {
 
 
 	$('#sync .tab-left').mouseenter(function() {
-		if ($('#sync .tab-text-left').css('left') == '-670px') {
-			$('#sync .tab-text-left').animate({left: '0px'}, 400);
+		if ($(window).width() > 1099) { // desktop
+			if ($('#sync .tab-text-left').css('left') == '-670px') {
+				$('#sync .tab-text-left').animate({left: '0px'}, 400);
+			}
 		}
+		else { // tablet
+			if ($('#sync .tab-text-left').css('left') == '-539px') {
+				$('#sync .tab-text-left').animate({left: '0px'}, 400);
+			}
+		}
+
 	});
 
 	$('#sync .slide-left').mouseleave(function() {
-		if ($('#sync .tab-text-left').css('left') == '0px') {
-			$('#sync .tab-text-left').animate({left: '-670px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#sync .tab-text-left').css('left') == '0px') {
+				$('#sync .tab-text-left').animate({left: '-670px'}, 400);
+			}
 		}
+		else {
+			if ($('#sync .tab-text-left').css('left') == '0px') {
+				$('#sync .tab-text-left').animate({left: '-539px'}, 400);
+			}			
+		}
+
 
 	});
 
 	$('#color .tab-right').mouseenter(function() {
-		if ($('#color .tab-text-right').css('right') == '-670px') {
-			$('#color .tab-text-right').animate({right: '0px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#color .tab-text-right').css('right') == '-670px') {
+				$('#color .tab-text-right').animate({right: '0px'}, 400);
+			}
 		}
+		else {
+			if ($('#color .tab-text-right').css('right') == '-539px') {
+				$('#color .tab-text-right').animate({right: '0px'}, 400);
+			}
+		}
+
 	});
 
 	$('#color .slide-right').mouseleave(function() {
-		if ($('#color .tab-text-right').css('right') == '0px') {
-			$('#color .tab-text-right').animate({right: '-670px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#color .tab-text-right').css('right') == '0px') {
+				$('#color .tab-text-right').animate({right: '-670px'}, 400);
+			}			
 		}
+		else {
+			if ($('#color .tab-text-right').css('right') == '0px') {
+				$('#color .tab-text-right').animate({right: '-539px'}, 400);
+			}
+		}
+
 
 	});
 
 	$('#touch .tab-left').mouseenter(function() {
-		if ($('#touch .tab-text-left').css('left') == '-670px') {
-			$('#touch .tab-text-left').animate({left: '0px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#touch .tab-text-left').css('left') == '-670px') {
+				$('#touch .tab-text-left').animate({left: '0px'}, 400);
+			}
 		}
+		else {
+			if ($('#touch .tab-text-left').css('left') == '-539px') {
+				$('#touch .tab-text-left').animate({left: '0px'}, 400);
+			}
+		}
+
 	});
 
 	$('#touch .slide-left').mouseleave(function() {
-		if ($('#touch .tab-text-left').css('left') == '0px') {
-			$('#touch .tab-text-left').animate({left: '-670px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#touch .tab-text-left').css('left') == '0px') {
+				$('#touch .tab-text-left').animate({left: '-670px'}, 400);
+			}
 		}
+		else {
+			if ($('#touch .tab-text-left').css('left') == '0px') {
+				$('#touch .tab-text-left').animate({left: '-539px'}, 400);
+			}
+		}
+
 
 	});
 
 	$('#app .tab-right').mouseenter(function() {
-		if ($('#app .tab-text-right').css('right') == '-670px') {
-			$('#app .tab-text-right').animate({right: '0px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#app .tab-text-right').css('right') == '-670px') {
+				$('#app .tab-text-right').animate({right: '0px'}, 400);
+			}
 		}
+		else {
+			if ($('#app .tab-text-right').css('right') == '-539px') {
+				$('#app .tab-text-right').animate({right: '0px'}, 400);
+			}
+		}
+
 	});
 
 	$('#app .slide-right').mouseleave(function() {
-		if ($('#app .tab-text-right').css('right') == '0px') {
-			$('#app .tab-text-right').animate({right: '-670px'}, 400);
+		if ($(window).width() > 1099) {
+			if ($('#app .tab-text-right').css('right') == '0px') {
+				$('#app .tab-text-right').animate({right: '-670px'}, 400);
+			}
 		}
+		else {
+			if ($('#app .tab-text-right').css('right') == '0px') {
+				$('#app .tab-text-right').animate({right: '-539px'}, 400);
+			}
+		}
+
 
 	});
 	
