e13e3ec4eac712ef935ff66b484b619862b4cda5
RAY <ray@wearhaus.com>
2015-08-08 17:02:34 -0700

    routed basic our story page

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ app = Flask(__name__)
 def main():
 	return render_template('index.html')
 
-# @app.route('/ourstory')
-# def ourstory():
-# 	return render_template('ourstory.html')
+@app.route('/ourstory')
+def ourstory():
+	return render_template('ourstory.html')
 
-# @app.route('/favicon.ico') 
-# def favicon(): 
-# 	return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 if __name__ == '__main__':
 	app.debug = False
