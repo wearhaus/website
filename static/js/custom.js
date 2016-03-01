81a56d3203b1d16e10df8a3b398d6d937cab86b5
Emily To <emilyto@emilys-air.attlocal.net>
2016-02-29 17:11:58 -0800

    added barely functioning color picker

--- static/js/custom.js
+++ static/js/custom.js
@@ -100,6 +100,67 @@ $(document).ready(function() {
 
 	});
 
+	$(function(){
+	    var bCanPreview = true; // can preview
+
+	    // create canvas and context objects
+	    var canvas = document.getElementById('picker');
+	    var ctx = canvas.getContext('2d');
+
+	    // drawing active image
+	    var image = new Image();
+	    image.onload = function () {
+	        ctx.drawImage(image, 0, 0, image.width, image.height); // draw the image on the canvas
+	    }
+
+	    // select desired colorwheel
+	    var imageSrc = 'images/colorwheel1.png';
+	    switch ($(canvas).attr('var')) {
+	        case '2':
+	            imageSrc = 'images/colorwheel2.png';
+	            break;
+	        case '3':
+	            imageSrc = 'images/colorwheel3.png';
+	            break;
+	        case '4':
+	            imageSrc = 'images/colorwheel4.png';
+	            break;
+	        case '5':
+	            imageSrc = '../static/img/colorwheel1.png';
+	            break;
+	    }
+	    image.src = imageSrc;
+
+	    $('#picker').mousemove(function(e) { // mouse move handler
+	            // get coordinates of current position
+	            var canvasOffset = $(canvas).offset();
+	            var canvasX = Math.floor(e.pageX - canvasOffset.left);
+	            var canvasY = Math.floor(e.pageY - canvasOffset.top);
+
+	            // get current pixel
+	            var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+	            var pixel = imageData.data;
+
+	            // update preview color
+	            var pixelColor = "rgb("+pixel[0]+", "+pixel[1]+", "+pixel[2]+")";
+	            $('.preview').css('backgroundColor', pixelColor);
+
+	            // update controls
+	            $('#rVal').val(pixel[0]);
+	            $('#gVal').val(pixel[1]);
+	            $('#bVal').val(pixel[2]);
+	            $('#rgbVal').val(pixel[0]+','+pixel[1]+','+pixel[2]);
+
+	            var dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
+	            $('#hexVal').val('#' + ('0000' + dColor.toString(16)).substr(-6));
+	            if (pixelColor == 'rgb(0, 0, 0)') {
+	            	$('.color-preview').css('backgroundColor', '#fff');
+	            } else {
+	            	$('.color-preview').css('backgroundColor', pixelColor);
+	            }
+	    });
+	});
+
 	$('#sync .slide-left').mouseleave(function() {
 		if ($(window).width() > 1099) {
 			if ($('#sync .tab-text-left').css('left') == '0px') {
