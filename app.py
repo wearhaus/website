163437637ff7c2d465af9e6eca67382decf778d6
Richie Zeng <richzeng@gmail.com>
2017-02-20 04:53:33 -0800

    Removed blog section loading from server side to improve load times. Will replace with JS initializing later.

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import urllib2
-import json
-import HTMLParser
 import gettext
 import locale
 
-from flask import Flask, Response, jsonify, redirect, render_template, request
-
-from blog import backup_posts
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 lang_code = locale.getdefaultlocale()[0]
@@ -15,18 +10,7 @@ lang_code = locale.getdefaultlocale()[0]
 def main():
     lang = gettext.translation('index', './locale', languages=[lang_code])
     _ = lang.ugettext
-
-    html_parser = HTMLParser.HTMLParser()
-    backup_featured = {"posts": backup_posts}
-    try:
-        # check to see if we can fetch the latest blog posts
-        featured_posts_url_response = urllib2.urlopen("http://blog.wearhaus.com/?json=get_recent_posts", timeout=5)
-        # make sure the response is a true json formatted string
-        featured_posts = json.loads(html_parser.unescape(featured_posts_url_response.read()))
-    except (urllib2.HTTPError, urllib2.URLError, ValueError):
-        featured_posts = backup_featured
-        pass
-    return render_template('index.html', featured_posts = featured_posts, _ = _)
+    return render_template('index.html', _ = _)
 
 
 @app.route('/ourstory')
@@ -69,5 +53,5 @@ def google():
     return render_template('google24c31f1e96a5aa9d.html')
 
 if __name__ == '__main__':
-    app.debug = False
+    app.debug = True
     app.run()
